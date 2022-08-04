d36fb1cc942faf74160b6beb38e3be9fc11bf7cc
Luna Hazuki <LunaHazuki2006@outlook.com>
2022-08-04 09:56:13 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const btnSend = document.getElementById("send-petition")
+
+btnSend?.addEventListener("click", async () => {
+    console.log("enviando petición...");
+    const petitionInput = document.getElementById("petition")
+    const questionInput = document.getElementById("question")
+
+    if (petitionInput.value === "" || questionInput.value === "") {
+        alert("Ambos campos deben estar llenos")
+        return
+    }
+
+    // enviar la petición
+    const dataToSend = {
+        answer: "",
+        client: "web", 
+        language: "es", 
+        number: 1, 
+        petition: petitionInput.value, 
+        question: petitionInput.value
+    }
+    console.log(dataToSend);
+
+    try {
+        const response = await fetch("https://peter-answers.appspot.com/_ah/api/paApi/v1/questions?alt=json", {
+            method: "POST", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataToSend)
+        })
+        const data = await response.json()
+        console.log(data.answer);
+        const answer = document.getElementById("answer")
+        answer.textContent = data.answer
+    } catch (error) {
+        alert(error)
+        console.error(error);
+    }
+});
